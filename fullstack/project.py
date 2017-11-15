ea4f497ec4908b38c640142291783ba1b9267eb6
florimondmanca <florimond.manca@gmail.com>
2017-11-15 21:00:43 +0100

    create Flask project and add list and add item views

---
+++ fullstack/project.py
@@ -0,0 +1,57 @@
+"""Simple Flask project for restaurant menus."""
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
+def create_db():
+    """Create a database session and return it."""
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind=engine)
+    return DBSession()
+
+
+app = Flask(__name__)
+db = create_db()
+
+
+@app.route('/restaurants/<int:restaurant_id>/items')
+def show_menu(restaurant_id):
+    """List the items of a restaurant."""
+    restaurant = db.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = db.query(MenuItem).filter_by(restaurant=restaurant)
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+
+@app.route('/restaurants/<int:restaurant_id>/items/add/',
+           methods=['GET', 'POST'])
+def add_item(restaurant_id):
+    """Add an item to a restaurant's menu."""
+    restaurant = db.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method == 'POST':
+        name = request.form['name']
+        item = MenuItem(name=name, restaurant=restaurant)
+        db.add(item)
+        db.commit()
+        return redirect(url_for('show_menu', restaurant_id=restaurant_id))
+    return render_template('add_item.html', restaurant=restaurant)
+
+
+@app.route('/restaurants/<int:restaurant_id>/items/<int:item_id>/edit/')
+def edit_item(restaurant_id, item_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurants/<int:restaurant_id>/items/<int:item_id>/delete/')
+def delete_item(restaurant_id, item_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
